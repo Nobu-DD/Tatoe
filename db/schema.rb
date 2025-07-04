0fe5ff340fb6acacc6f7d0c33a142bbf40b420d2
nobuhiro-sato <n.s.mh256@gmail.com>
2025-07-04 13:43:58 +0900

    add:お題テーブル、モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_02_044421) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_04_042816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_02_044421) do
     t.datetime "updated_at", null: false
     t.index ["genre_id"], name: "index_my_genres_on_genre_id"
     t.index ["user_id"], name: "index_my_genres_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "title", null: false
+    t.text "description"
+    t.datetime "published_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
