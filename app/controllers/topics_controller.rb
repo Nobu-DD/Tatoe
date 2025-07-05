651a6abc806c84720b00894b125ab5c04385e47d
nobuhiro-sato <n.s.mh256@gmail.com>
2025-07-06 01:38:53 +0900

    add:お題新規登録用ページ作成、登録処理作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,22 +1,26 @@
 class TopicsController < ApplicationController
   def new
-    @topic = current_user.topics.build
+    @topic = Topic.new
+    3.times { @topic.hints.build }
   end
 
   def create
     @topic = current_user.topics.new(topic_params)
+    @topic.genre_names = params[:topic][:genre_names]
     raise
     if @topic.save
-      @topic.genres_save(genre_params)
+      redirect_to @topic, notice: t("topics.create.success")
+    else
+      render :new, status: :unprocessable_entity, notice: t("spots.create.failure")
     end
   end
+
   private
 
   def topic_params
-    params.require(:topic).permit(:title, :description)
-  end
-
-  def genre_params
-    params[:genre][:genre_names].split('').uniq
+    params.require(:topic).permit(
+      :title, :description,
+      hints_attributes: [:id, :body, :_destroy]
+    )
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ApplicationRecord
   include PublishedAtSettable
+  after_save :assign_genres
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 255 }
@@ -8,10 +9,16 @@ class Topic < ApplicationRecord
   has_many :hints, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :topic_genres, dependent: :destroy
+  has_many :genres, through: :topic_genres
   belongs_to :user
 
-  def genres_save(genres)
-    
-  end
+  attr_accessor :genre_names
+  accepts_nested_attributes_for :hints, allow_destroy: true
 
+  def assign_genres
+    return if genre_names.blank?
+
+    names = genre_names.split(/[[:space:],、\/]|　+/).reject(&:blank?).uniq
+    self.genres = names.map { |name| Genre.find_or_create_by(name: name) }
+  end
 end
