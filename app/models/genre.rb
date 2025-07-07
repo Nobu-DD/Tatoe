911822cc52d0abd854c06f552db1a9bec16264b9
nobuhiro-sato <n.s.mh256@gmail.com>
2025-07-07 17:54:46 +0900

    add:フラッシュメッセージ日本語化対応

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,10 @@
 class Genre < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 10 }
+
   has_many :my_genres, dependent: :destroy
   has_many :users, through: :my_genres
   has_many :topic_genres, dependent: :destroy
   has_many :topics, through: :topic_genres
+
 end
