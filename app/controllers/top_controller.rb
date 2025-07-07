bdcfec033e7a33c711da7d197d7a65afcdd4220b
nobuhiro-sato <n.s.mh256@gmail.com>
2025-07-07 16:34:41 +0900

    fix:lintチェック修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
     @topics = Topic.includes(:user, :genres, :hints)
