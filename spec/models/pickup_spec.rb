0cbb199a65914099d05b09118d77f1912b69a7fd
nobuhiro-sato <n.s.mh256@gmail.com>
2025-08-22 19:56:11 +0900

    add:モデルスペック：pickup_specコード記入

--- spec/models/pickup_spec.rb
+++ spec/models/pickup_spec.rb
@@ -2,16 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Pickup", type: :model do
   describe 'バリデーションチェック' do
+    before do
+      @user = create(:user)
+      @topic = build(:topic)
+      @topic.user_id = @user.id
+      @topic.save
+      @pickup = build(:pickup)
+      @pickup.topic_id = @topic.id
+    end
     context 'お題に対してピックアップ期間を指定できる' do
       it 'ピックアップが(開始時間 < 終了時間)で入力' do
+        expect(@pickup).to be_valid
       end
     end
     context 'お題に対してピックアップ期間を指定出来ない' do
       it 'ピックアップが(開始時間 > 終了時間)で入力' do
+        @pickup.start_at = @pickup.end_at + 1.day
+        expect(@pickup).to be_invalid
+        expect(@pickup.errors[:start_at]).to eq(["は終了日時より前に設定してください"])
       end
       it '開始時間が入力されていない' do
+        @pickup.start_at = nil
+        expect(@pickup).to be_invalid
+        expect(@pickup.errors[:start_at]).to eq(["を入力してください"])
       end
       it '終了時間が入力されていない' do
+        @pickup.end_at = nil
+        expect(@pickup).to be_invalid
+        expect(@pickup.errors[:end_at]).to eq(["を入力してください"])
       end
     end
   end
