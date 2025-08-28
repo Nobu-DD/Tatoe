22e9a6339b568b4c862de3e4fcd22f2b0b8059da
nobuhiro-sato <n.s.mh256@gmail.com>
2025-08-28 18:11:24 +0900

    add:mypage_spec.rbファイル作成・テスト定義まで

---
+++ spec/system/mypage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Mypage", type: :system do
+  describe 'newアクション' do
+    it 'フッターのマイページボタンを押すと、マイページに遷移する' do
+
+    end
+    it '未認証だった場合、ログインページに遷移する' do
+
+    end
+  end
+end
