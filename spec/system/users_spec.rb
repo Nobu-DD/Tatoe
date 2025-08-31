33e4ad20c1753b2834152274774d8992cafb6f87
nobuhiro-sato <n.s.mh256@gmail.com>
2025-08-31 13:50:05 +0900

    add:ユーザー編集用テストケースフローチャート定義

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,103 +1,125 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  describe "newアクション" do
-    context "正常系" do
-      it "新規登録画面に遷移する" do
-        # パスを指定して遷移する
-        visit('/users/sign_up')
-        # 新規登録画面に遷移しているか確認
-        expect(page).to have_current_path(new_user_registration_path)
+  describe "認証前" do
+    describe "newアクション" do
+      context "正常系" do
+        it "新規登録画面に遷移する" do
+          # パスを指定して遷移する
+          visit('/users/sign_up')
+          # 新規登録画面に遷移しているか確認
+          expect(page).to have_current_path(new_user_registration_path)
+        end
+      end
+    end
+    describe "createアクション" do
+      before do
+        # パスを指定して新規登録ページに遷移する
+        visit(new_user_registration_path)
+        # 「新規登録」文字列があるか検証(h2)
+        expect(page).to have_selector('h2', text: '新規登録')
+      end
+      context "正常系" do
+        it "入力必須のフォームに値を入れて登録出来る" do
+          # 各フォームに必要事項を記入する
+          fill_in 'ユーザー名', with: '例え太郎'
+          fill_in 'メールアドレス', with: 'tatoe@example.com'
+          fill_in 'パスワード', with: 'password'
+          fill_in '確認用パスワード', with: 'password'
+          # 登録ボタンを押す
+          click_button '新規登録'
+          # トップページに遷移し、「登録完了のフラッシュメッセージ」が表示されているか検証
+          expect(page).to have_current_path(root_path)
+          expect(page).to have_selector('.alert-info', text: '新規登録が完了しました。')
+          # 登録したユーザーで認証済みかどうか検証(ログアウトボタンが表示されているか確認)
+          expect(page).to have_link('ログアウト')
+        end
+      end
+
+      context "異常系" do
+        it "ユーザー名を入れていない時" do
+          fill_in 'メールアドレス', with: 'tatoe@example.com'
+          fill_in 'パスワード', with: 'password'
+          fill_in '確認用パスワード', with: 'password'
+          click_button '新規登録'
+          # トップページに遷移し、「登録完了のフラッシュメッセージ」が表示されているか検証
+          expect(page).to have_current_path(new_user_registration_path)
+          expect(page).to have_selector('.alert-error', text: 'ニックネームを入力してください')
+          # 登録したユーザーで認証済みかどうか検証(ログアウトボタンが表示されているか確認)
+          expect(page).to have_link('ログイン')
+          expect(page).to have_link('新規登録')
+        end
+
+        it "メールアドレスを入れていない時" do
+          fill_in 'ユーザー名', with: '例え太郎'
+          fill_in 'パスワード', with: 'password'
+          fill_in '確認用パスワード', with: 'password'
+          click_button '新規登録'
+          expect(page).to have_current_path(new_user_registration_path)
+          expect(page).to have_selector('.alert-error', text: 'メールアドレスを入力してください')
+          expect(page).to have_link('ログイン')
+          expect(page).to have_link('新規登録')
+        end
+
+        it "パスワードを入れていない時" do
+          fill_in 'ユーザー名', with: '例え太郎'
+          fill_in 'メールアドレス', with: 'tatoe@example.com'
+          fill_in '確認用パスワード', with: 'password'
+          click_button '新規登録'
+          expect(page).to have_current_path(new_user_registration_path)
+          expect(page).to have_selector('.alert-error', text: 'パスワードを入力してください')
+          expect(page).to have_link('ログイン')
+          expect(page).to have_link('新規登録')
+        end
+
+        it "パスワードと確認用パスワードが一致しない時" do
+          fill_in 'ユーザー名', with: '例え太郎'
+          fill_in 'メールアドレス', with: 'tatoe@example.com'
+          fill_in 'パスワード', with: 'password'
+          fill_in '確認用パスワード', with: 'password2'
+          click_button '新規登録'
+          expect(page).to have_current_path(new_user_registration_path)
+          expect(page).to have_selector('.alert-error', text: '確認用パスワードとパスワードの入力が一致しません')
+          expect(page).to have_link('ログイン')
+          expect(page).to have_link('新規登録')
+        end
       end
     end
   end
-  describe "editアクション" do
-    it "マイページの編集ボタンを押すと、編集ページに遷移する" do
-
-    end
-  end
-  describe "createアクション" do
+  describe "認証後" do
     before do
-      # パスを指定して新規登録ページに遷移する
-      visit(new_user_registration_path)
-      # 「新規登録」文字列があるか検証(h2)
-      expect(page).to have_selector('h2', text: '新規登録')
+      @user = create(:user)
     end
-    context "正常系" do
-      it "入力必須のフォームに値を入れて登録出来る" do
-        # 各フォームに必要事項を記入する
-        fill_in 'ユーザー名', with: '例え太郎'
-        fill_in 'メールアドレス', with: 'tatoe@example.com'
-        fill_in 'パスワード', with: 'password'
-        fill_in '確認用パスワード', with: 'password'
-        # 登録ボタンを押す
-        click_button '新規登録'
-        # トップページに遷移し、「登録完了のフラッシュメッセージ」が表示されているか検証
-        expect(page).to have_current_path(root_path)
-        expect(page).to have_selector('.alert-info', text: '新規登録が完了しました。')
-        # 登録したユーザーで認証済みかどうか検証(ログアウトボタンが表示されているか確認)
-        expect(page).to have_link('ログアウト')
+    describe "editアクション" do
+      it "マイページの編集ボタンを押すと、編集ページに遷移する" do
+        # マイページに遷移する
+        # マイページの編集ボタンを押す
+        # マイページの編集ページに遷移しているか検証
       end
     end
-
-    context "異常系" do
-      it "ユーザー名を入れていない時" do
-        fill_in 'メールアドレス', with: 'tatoe@example.com'
-        fill_in 'パスワード', with: 'password'
-        fill_in '確認用パスワード', with: 'password'
-        click_button '新規登録'
-        # トップページに遷移し、「登録完了のフラッシュメッセージ」が表示されているか検証
-        expect(page).to have_current_path(new_user_registration_path)
-        expect(page).to have_selector('.alert-error', text: 'ニックネームを入力してください')
-        # 登録したユーザーで認証済みかどうか検証(ログアウトボタンが表示されているか確認)
-        expect(page).to have_link('ログイン')
-        expect(page).to have_link('新規登録')
+    describe "updateアクション" do
+      context "正常系" do
+        it "全ての値を変更した後、マイページに遷移する" do
+        # マイページに直接遷移する
+        # マイページの各フォームに入力する
+        # 変更ボタンを押す
+        # マイページに遷移しているか検証
+        # フラッシュメッセージが表示されているか検証
+        # ユーザーの各項目が、編集ページで入れていた値になっているか検証
+        end
       end
-
-      it "メールアドレスを入れていない時" do
-        fill_in 'ユーザー名', with: '例え太郎'
-        fill_in 'パスワード', with: 'password'
-        fill_in '確認用パスワード', with: 'password'
-        click_button '新規登録'
-        expect(page).to have_current_path(new_user_registration_path)
-        expect(page).to have_selector('.alert-error', text: 'メールアドレスを入力してください')
-        expect(page).to have_link('ログイン')
-        expect(page).to have_link('新規登録')
-      end
-
-      it "パスワードを入れていない時" do
-        fill_in 'ユーザー名', with: '例え太郎'
-        fill_in 'メールアドレス', with: 'tatoe@example.com'
-        fill_in '確認用パスワード', with: 'password'
-        click_button '新規登録'
-        expect(page).to have_current_path(new_user_registration_path)
-        expect(page).to have_selector('.alert-error', text: 'パスワードを入力してください')
-        expect(page).to have_link('ログイン')
-        expect(page).to have_link('新規登録')
-      end
-
-      it "パスワードと確認用パスワードが一致しない時" do
-        fill_in 'ユーザー名', with: '例え太郎'
-        fill_in 'メールアドレス', with: 'tatoe@example.com'
-        fill_in 'パスワード', with: 'password'
-        fill_in '確認用パスワード', with: 'password2'
-        click_button '新規登録'
-        expect(page).to have_current_path(new_user_registration_path)
-        expect(page).to have_selector('.alert-error', text: '確認用パスワードとパスワードの入力が一致しません')
-        expect(page).to have_link('ログイン')
-        expect(page).to have_link('新規登録')
-      end
-    end
-  end
-  describe "updateアクション" do
-    context "正常系" do
-      it "全ての値を変更した後、マイページに遷移する" do
-
-      end
-    end
-    context "異常系" do
-      it "ユーザー名・メールアドレスが空欄の場合、フラッシュメッセージを返す" do
-        
+      context "異常系" do
+        it "ユーザー名・メールアドレスが空欄の場合、フラッシュメッセージを返す" do
+        # マイページに直接遷移する
+        # ユーザー名を空白にする
+        # 変更ボタンを押す
+        # 編集ページに遷移するか検証
+        # ユーザー名を入力していないというフラッシュメッセージを出力しているか検証
+        # メールアドレスを空白にする
+        # 変更ボタンを押す
+        # 編集ページに遷移するか検証
+        # メールアドレスを入力していないというフラッシュメッセージを出力しているか検証
+        end
       end
     end
   end
