f70d91b9a0cedf4e09e5647337e60f21ca31776d
nobuhiro-sato <n.s.mh256@gmail.com>
2025-09-05 21:14:23 +0900

    fix:ドメイン指定の記述を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,10 +96,8 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "tatoe.net",     # Allow requests from example.com
-    "www.tatoe.com" # Allow requests from subdomains like `www.example.com`
-  ]
+  config.hosts << "tatoe.net"     # Allow requests from example.com
+  config.hosts << "www.tatoe.com" # Allow requests from subdomains like `www.example.com`
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
