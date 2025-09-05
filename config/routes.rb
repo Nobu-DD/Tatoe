8b4326bf19a82e81b319c4009536c65429eb6ba2
nobuhiro-sato <n.s.mh256@gmail.com>
2025-09-05 11:53:26 +0900

    add:letter_openerルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "top#index"
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
