61d70b1221ea55024c73517b2415083a8692c25f
nobuhiro-sato <n.s.mh256@gmail.com>
2025-09-05 19:43:01 +0900

    fix:hostsのドメイン名を一部修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     "tatoe.net",     # Allow requests from example.com
-    /.*\.tatoe\.net/ # Allow requests from subdomains like `www.example.com`
+    "www.tatoe.com" # Allow requests from subdomains like `www.example.com`
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
