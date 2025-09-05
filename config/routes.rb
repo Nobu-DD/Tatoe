90931f57ea6130bd817531a6f581fec6656a9bdf
nobuhiro-sato <n.s.mh256@gmail.com>
2025-09-05 16:40:47 +0900

    add:マイページのパスワード変更機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+
+  devise_scope :user do
+    get "users/edit_password", to: "users/registrations#edit_password"
+    patch "users/update_password", to: "users/registrations#update_password"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
