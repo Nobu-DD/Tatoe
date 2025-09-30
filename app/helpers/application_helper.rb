7ab3ce15906c240463e76df30d802a1a9408c5f5
nobuhiro-sato <n.s.mh256@gmail.com>
2025-09-30 21:52:54 +0900

    各ページtitleにcontent_forを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      charset: "utf-8",
+      description: "Tatoeは普段接点のない趣味や文化、職業などを、ユーザー同士で“例え”を使って共感を生むアプリです。「知ってほしい趣味、文化、職業」をお題として登録し、それに対して他のユーザーが「自分の得意分野」で例えていく、ユーザー参加型例え大喜利アプリになります。"
+    }
+  end
+
+  def page_title(title = "")
+    base_title = "Tatoe - 例えでつながる共感アプリ"
+    title.present? ? "#{title} | #{base_title}" : base_title
+  end
 end
