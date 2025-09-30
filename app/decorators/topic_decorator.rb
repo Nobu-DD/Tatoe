d4546694ef8ee23e3d17a94941b4dc3e2063ecd4
nobuhiro-sato <n.s.mh256@gmail.com>
2025-09-30 14:52:50 +0900

    add:x_share_encodeメソッド作成

--- app/decorators/topic_decorator.rb
+++ app/decorators/topic_decorator.rb
@@ -8,4 +8,12 @@ class TopicDecorator < Draper::Decorator
       hints: object.hints.map(&:body)
     }.to_json
   end
+
+  def x_share_encode
+    message = "お題を投稿しました！例えてみてください！"
+    genres = object.genres.map{|genre| "#" + genre}.join(' ')
+    url = "https://tatoe.net/topics/#{object.id}"
+
+    return URI.encode_www_form_component('#{message} #{genres} #{url}')
+  end
 end
