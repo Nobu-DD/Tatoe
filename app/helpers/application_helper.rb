614b32f5eddc999f8d2c76445cdde766db985fbf
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-01 17:02:50 +0900

    add:OGP画像設定(本番環境で確認)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,20 @@ module ApplicationHelper
   def default_meta_tags
     {
       charset: "utf-8",
-      description: "Tatoeは普段接点のない趣味や文化、職業などを、ユーザー同士で“例え”を使って共感を生むアプリです。「知ってほしい趣味、文化、職業」をお題として登録し、それに対して他のユーザーが「自分の得意分野」で例えていく、ユーザー参加型例え大喜利アプリになります。"
+      description: "Tatoeは普段接点のない趣味や文化、職業などを、ユーザー同士で“例え”を使って共感を生むアプリです。「知ってほしい趣味、文化、職業」をお題として登録し、それに対して他のユーザーが「自分の得意分野」で例えていく、ユーザー参加型例え大喜利アプリになります。",
+      canonical: request.original_url,
+      og: {
+        title: page_title,
+        description: "Tatoeは普段接点のない趣味や文化、職業などを、ユーザー同士で“例え”を使って共感を生むアプリです。",
+        type: "website",
+        image: image_url("Tatoe_OGP"),
+        url: request.original_url,
+        locale: "ja-JP"
+      },
+      twitter: {
+        card: "summary_large_image",
+        site: "@nobu1362622"
+      }
     }
   end
 
