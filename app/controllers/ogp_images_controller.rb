2e0bd38831cb122a68b8a9f31f832b0dea010d24
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-01 22:20:05 +0900

    add:動的OGP処理記述

---
+++ app/controllers/ogp_images_controller.rb
@@ -0,0 +1,7 @@
+class OgpImagesController < ApplicationController
+  def show_topic
+    topic = Topic.find(params[:id])
+    image = OgpCreatorService.build(topic.title).tempfile.open.read
+    send_data image, type: "image/png", disposition: "inline"
+  end
+end
---
+++ app/services/ogp_creator.rb
@@ -0,0 +1,28 @@
+class OgpCreatorService
+  require "mini_magick"
+
+  BASE_IMAGE_PATH = image_url("Tatoe_OGP.png")
+  GRAVITY = "center"
+  TEXT_POSITION = "0,0"
+  TEXT_COLOR = "white"
+  FONT_SIZE = 65
+  INDENTION_COUNT = 15
+  ROW_LIMIT = 3
+
+  def self.build(text)
+    text = prepare_text(text)
+    image = MiniMagick::Image.open(BASE_IMAGE_PATH)
+    image.combine_options do |config|
+      config.fill TEXT_COLOR
+      config.gravity GRAVITY
+      config.pointsize FONT_SIZE
+      config.draw "text #{TEXT_POSITION} '#{text}'"
+    end
+  end
+
+  private
+
+  def self.prepare_text(text)
+    text.scan(/.{1,#{INDENTION_COUNT}}/)[0...ROW_LIMIT].join("\n")
+  end
+end
