cbdf01eb8c14f841c4bca76498062a94faf422da
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-04 00:34:44 +0900

    add:コメント投稿機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      respond_to do |format|
-        render turbo_stream: turbo_stream.prepend(:comments)
-        end
-      end
+    @new_comment = current_user.comments.build(comment_params)
+    @new_comment[:answer_id] = params[:answer_id]
+    @topic = Topic.find(params[:topic_id])
+    @answer = @new_comment.answer
+    @reactions = Reaction.all
+    @comments = @answer.comments.order(published_at: :desc)
+    if @new_comment.save
+      redirect_to topic_answer_path(@topic, @answer), notice: t("comment.create.success")
+    else
+      render "answers/show", status: :unprocessable_entity
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :answer_id)
+    params.require(:comment).permit(:body)
   end
-
 end
