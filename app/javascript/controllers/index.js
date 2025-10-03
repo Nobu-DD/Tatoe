a27cf8414b250271ffeea2fe6d4c490a11ca0dfa
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-04 02:26:19 +0900

    add:コメントフォームプルダウン実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,3 +12,6 @@ application.register("topic", GeminiTopicController)
 
 import GeminiAnswerController from "./gemini_answer_controller"
 application.register("answer", GeminiAnswerController)
+
+import CommentController from "./comment_controller"
+application.register("comment", CommentController)
