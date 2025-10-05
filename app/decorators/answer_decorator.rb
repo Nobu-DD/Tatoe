fc3dfa78be375bafb1b114251ab1c91d4d44584c
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-05 16:31:28 +0900

    fix:answer_decorator内容除容

--- app/decorators/answer_decorator.rb
+++ app/decorators/answer_decorator.rb
@@ -1,16 +1,6 @@
 class AnswerDecorator < Draper::Decorator
   delegate_all
 
-  def x_share_encode
-    message = 
-    genres = object.topic.genres.limit(5).map { |genre| "##{genre.name}" }.join(" ")
-    # 画像OGPのメタデータを格納したURLを設定する
-    url = "https://tatoe.net/topics/#{object.id}"
-
-    encode_text = URI.encode_www_form_component("#{message} #{genres}")
-    "https://twitter.com/share?text=#{encode_text}&url=#{url}"
-  end
-
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
