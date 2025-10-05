6b48dd5c8a99b4e086ea082243c07db36f69d49f
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-05 12:02:54 +0900

    add:ogp_image処理内容記述(例え用OGP)

--- app/controllers/ogp_images_controller.rb
+++ app/controllers/ogp_images_controller.rb
@@ -2,7 +2,13 @@ class OgpImagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show_topic]
   def show_topic
     topic = Topic.find(params[:id])
-    image = OgpCreatorService.build(topic.title).tempfile.open.read
+    image = OgpCreatorService.topic_build(topic.title).tempfile.open.read
     send_data image, type: "image/png", disposition: "inline"
+  end
+
+  def show_answer
+    topic = Topic.includes(:answers).find(params[:topic_id])
+    answer = topic.answers.find { |answer| answer.id == params[:answer_id] }
+    image = OgpCreatorService.answer_build(topic.title, answer.body)
   end
 end
--- app/services/ogp_creator_service.rb
+++ app/services/ogp_creator_service.rb
@@ -3,22 +3,40 @@ class OgpCreatorService
 
   BASE_IMAGE_PATH = "./app/assets/images/Tatoe_OGP.png"
   FONT = "./app/assets/fonts/Noto_Sans_JP/static/NotoSansJP-Bold.ttf"
-  GRAVITY = "center"
   TEXT_POSITION = "0,0"
   TEXT_COLOR = "white"
-  FONT_SIZE = 65
   INDENTION_COUNT = 15
   ROW_LIMIT = 3
 
-  def self.build(text)
-    text = prepare_text(text)
+  def self.topic_build(topic_title)
+    topic_text = prepare_text(topic_title)
     image = MiniMagick::Image.open(BASE_IMAGE_PATH)
     image.combine_options do |config|
+      # お題タイトル
       config.fill TEXT_COLOR
       config.font FONT
-      config.gravity GRAVITY
-      config.pointsize FONT_SIZE
-      config.draw "text #{TEXT_POSITION} '#{text}'"
+      config.gravity "center"
+      config.pointsize 65
+      config.draw "text #{TEXT_POSITION} '#{topic_text}'"
+    end
+  end
+
+  def self.answer_build(topic_title, answer_body)
+    topic_text, answer_text = [topic_title, answer_body].map{ |text| prepare_text(text) }
+    image = MiniMagick::Image.open(BASE_IMAGE_PATH)
+    image.combine_options do |config|
+      # お題タイトル
+      config.fill TEXT_COLOR
+      config.font FONT
+      config.gravity "north"
+      config.pointsize 50
+      config.draw "text #{TEXT_POSITION} '#{topic_text}'"
+       # 例え内容
+      config.fill TEXT_COLOR
+      config.font FONT
+      config.gravity "center"
+      config.pointsize 65
+      config.draw "text #{TEXT_POSITION} '#{answer_text}'"
     end
   end
 
