179f7b61a4568735f5a76add3f8b4eea2fb9814e
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-06 19:00:17 +0900

    fix:ログイン前のコメント投稿ボタン文章変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :store_user_location!, only: [:new]
   # GET /resource/sign_in
   def new
     super
@@ -18,6 +18,14 @@ class Users::SessionsController < Devise::SessionsController
     super
   end
 
+  private
+
+  def store_user_location!
+    # トップページ（root_path）以外からログイン画面に遷移した場合ログイン後、元のページに遷移する
+    if params[:redirect_url].present?
+      store_location_for(:user, params[:redirect_url])
+    end
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
