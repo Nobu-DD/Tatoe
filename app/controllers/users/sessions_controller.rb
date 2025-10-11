53c554be28d674a46e6ab3e0ed952993214cb68a
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-11 23:34:13 +0900

    fix:rubocop修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :set_custom_return_to, only: [:new]
+  before_action :set_custom_return_to, only: [ :new ]
   # GET /resource/sign_in
   def new
     super
