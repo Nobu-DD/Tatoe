64fdf49865d081d04ba075b10f3ce7896c82ea4a
nobuhiro-sato <n.s.mh256@gmail.com>
2025-10-12 16:41:28 +0900

    fix:お題詳細ページ修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -16,9 +16,9 @@ class AnswersController < ApplicationController
 
   def create
     @answer = current_user.answers.build(answer_params)
-    @topic = Topic.includes(:user, :genres, :hints, :answers).find(params[:topic_id])
     @reactions = Reaction.all
     if @answer.save
+      @topic = Topic.includes(:user, :genres, :hints, :answers).find(params[:topic_id])
       flash.now.notice = t("answer.create.success")
     else
       render :new, status: :unprocessable_entity, notice: t("spots.create.failure")
@@ -45,6 +45,7 @@ class AnswersController < ApplicationController
   def destroy
     @answer = current_user.answers.find(params[:id])
     @answer.destroy!
+    @answers = Topic.includes(:answers).find(params[:topic_id]).answers
     flash.now.notice = t("answer.deleted.success")
   end
 
