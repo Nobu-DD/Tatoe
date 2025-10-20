844d8e2c3f8e3e1b103ee8ebde69330b64bca328
Nobu-DD <n.s.mh256@gmail.com>
2025-10-20 14:08:21 +0900

    add:並び替えを選択した時、自動で読み込みされる処理を記述

--- app/javascript/controllers/topics_search_controller.js
+++ app/javascript/controllers/topics_search_controller.js
@@ -2,19 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="topics-search"
 export default class extends Controller {
-  static targets = ["details_bottom", "icon_arrow_bottom", "advanced_search"]
+  static targets = ["details_button", "icon_arrow_button", "advanced_search", "form_submit"]
 
   show_details() {
     console.log("詳細画面展開ィィィィィィ！！！")
-    this.icon_arrow_bottomTarget.className = "absolute -top-[3px] inline-block box-border w-[13px] h-[13px] border-solid border-t-3 border-r-3 border-cyan-400 -rotate-45";
+    this.icon_arrow_buttonTarget.className = "absolute -top-[3px] inline-block box-border w-[13px] h-[13px] border-solid border-t-3 border-r-3 border-cyan-400 -rotate-45";
     this.advanced_searchTarget.classList.remove("hidden");
-    this.details_bottomTarget.dataset.action = "click->topics-search#close_details";
+    this.details_buttonTarget.dataset.action = "click->topics-search#close_details";
   }
 
   close_details() {
     console.log("詳細画面閉じィィィィィィ！！！")
-    this.icon_arrow_bottomTarget.className = "absolute -top-[10px] inline-block box-border w-[13px] h-[13px] border-solid border-t-3 border-r-3 border-cyan-400 rotate-135";
+    this.icon_arrow_buttonTarget.className = "absolute -top-[10px] inline-block box-border w-[13px] h-[13px] border-solid border-t-3 border-r-3 border-cyan-400 rotate-135";
     this.advanced_searchTarget.classList.add("hidden");
-    this.details_bottomTarget.dataset.action = "click->topics-search#show_details";
+    this.details_buttonTarget.dataset.action = "click->topics-search#show_details";
+  }
+
+  sort_change() {
+    console.log("領域展開...")
+    this.form_submitTarget.click()
   }
 }
