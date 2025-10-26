78f87703f2e76a1ae423612f742d078bfe8a6888
Nobu-DD <n.s.mh256@gmail.com>
2025-10-26 22:54:47 +0900

    add:例え対象切り替えボタン実装

--- app/javascript/controllers/rankings_controller.js
+++ app/javascript/controllers/rankings_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="rankings"
 export default class extends Controller {
-  static targets = ["general_button", "week_button", "month_button", "like_button", "answer_button", "current_period", "current_sort"]
+  static targets = ["general_button", "week_button", "month_button", "like_button", "answer_button", "current_period", "current_sort", "all_button", "empathy_button", "consent_button", "smile_button" ]
 
   period_change(event) {
     console.log("ランキングを期間で切り替え");
@@ -24,7 +24,7 @@ export default class extends Controller {
     this.current_periodTarget.value = clickedElement.dataset.currentPeriod
   }
 
-  sort_change(event) {
+  topics_sort_change(event) {
     const clickedElement = event.currentTarget;
     const beforeContent = clickedElement.dataset.beforeContent;
     const afterContent = clickedElement.dataset.afterContent;
@@ -33,6 +33,24 @@ export default class extends Controller {
     this[`${afterContent}_buttonTarget`].className = "btn btn-info rounded-xl pointer-events-none";
     this[`${beforeContent}_buttonTarget`].dataset.beforeContent = `${afterContent}`;
     delete clickedElement.dataset.beforeContent
+
+    this.current_sortTarget.value = `${afterContent}s_count desc`
+  }
+
+  answers_sort_change(event) {
+    const contentTargets = [this.all_buttonTarget, this.empathy_buttonTarget, this.consent_buttonTarget, this.smile_buttonTarget];
+    const clickedElement = event.currentTarget;
+    const beforeContent = clickedElement.dataset.beforeContent;
+    const afterContent = clickedElement.dataset.afterContent;
+
+    this[`${beforeContent}_buttonTarget`].className = "btn btn-outline btn-info rounded-xl";
+    this[`${afterContent}_buttonTarget`].className = "btn btn-info rounded-xl pointer-events-none";
+    contentTargets.forEach(target => {
+      if (!(target.dataset.rankingsTarget == `${afterContent}_button`)) {
+        target.dataset.beforeContent = `${afterContent}`;
+      }
+    })
+    delete clickedElement.dataset.beforeContent;
 
     this.current_sortTarget.value = `${afterContent}s_count desc`
   }
