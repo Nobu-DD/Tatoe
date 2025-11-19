6c9d3eb2aea864cf542498a9740d875a1bcf9083
Nobu-DD <n.s.mh256@gmail.com>
2025-11-19 18:22:46 +0900

    add:answer_reactionsの保存

--- spec/models/answer_reactions_spec.rb
+++ spec/models/answer_reactions_spec.rb
@@ -6,13 +6,14 @@ RSpec.describe "AnswerReaction", type: :model do
       @user = create(:user)
       @topic = create(:topic, user_id: @user.id)
       @answer = create(:answer, user_id: @user.id, topic_id: @topic.id)
-      @reactions = create(:reaction, :empathy)
-      binding.pry
+      @reaction_empathy = create(:reaction, :empathy)
+      @reaction_consent = create(:reaction, :consent)
+      @reaction_smaile = create(:reaction, :smile)
     end
 
     context '正常系：例えに対してリアクション(3つ)登録ができる' do
       it '何も登録されていない時' do
-
+        
       end
     end
   end
