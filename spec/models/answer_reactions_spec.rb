8f148a94b73af0d600759e709396eecc27f16b43
Nobu-DD <n.s.mh256@gmail.com>
2025-11-26 23:55:55 +0900

    add:answer_reactionのテスト追加(共感が既に登録済みの時、納得を登録する時)

--- spec/models/answer_reactions_spec.rb
+++ spec/models/answer_reactions_spec.rb
@@ -18,6 +18,11 @@ RSpec.describe "AnswerReaction", type: :model do
         # expectでbe_validに引っかからないか検証
         expect(@answer_empathy).to be_valid
       end
+      it '共感が既に登録済みの時、納得を登録する時' do
+        @answer_empathy = create(:answer_reaction, user_id: @user.id, answer_id: @answer.id, reaction_id: @reaction_empathy.id)
+        duplicate_reaction = build(:answer_reaction, user_id: @user.id, answer_id: @answer.id, reaction_id: @reaction_empathy.id)
+        expect(duplicate_reaction).to be_invalid
+      end
     end
   end
 end
